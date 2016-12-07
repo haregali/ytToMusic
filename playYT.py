549df8a56de79f1716863bb12a531648bf88c276
HareeshGali <hgali@purdue.edu>
2016-12-06 20:00:19 -0500

    INITIAL COMMIT

---
+++ playYT.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+from __future__ import unicode_literals
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.tools import argparser
+import youtube_dl
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = "AIzaSyAgAJ3GQHFNSv-tMUuhehojF-TlnLI_P-0  "
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+x = ""
+def youtube_search(options):
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+
+  # Call the search.list method to retrieve results matching the specified
+  # query term.
+  search_response = youtube.search().list(
+    q=options.q,
+    part="id,snippet",
+    maxResults=options.max_results
+  ).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                 search_result["id"]["videoId"]))
+    elif search_result["id"]["kind"] == "youtube#channel":
+      channels.append("%s (%s)" % (search_result["snippet"]["title"],
+                                   search_result["id"]["channelId"]))
+    elif search_result["id"]["kind"] == "youtube#playlist":
+      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+                                    search_result["id"]["playlistId"]))
+
+  x = videos[0].split("(")[2]
+  x = x[:len(x)-1]
+  return x
+def my_hook(d):
+  if d['status'] == 'finished':
+      print('Done downloading, now converting ...')
+
+if __name__ == "__main__":
+  argparser.add_argument("--q", help="Search term", default="Google")
+  argparser.add_argument("--max-results", help="Max results", default=25)
+  args = argparser.parse_args()
+  x = ""
+
+  try:
+    x = youtube_search(args)
+  except HttpError, e:
+    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+
+
+
+  ydl_opts = {
+      'format': 'bestaudio/best',       
+      'outtmpl': 'music.mp3',        
+      'noplaylist' : True,        
+      'progress_hooks': [my_hook],  
+  }
+
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+      print x
+      ydl.download(['https://www.youtube.com/watch?v=' + x])
+  print x
